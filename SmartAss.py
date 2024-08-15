5a52eba08b4fe1f771a60a6dc7ba01e8289eeb11
Paul Slinkard <slinkardpaul50@gmail.com>
2024-08-15 14:11:42 -0500

    Created branch for adding data to Response.
    Also corrected a typo in Response.

--- SmartAss.py
+++ SmartAss.py
@@ -8,7 +8,7 @@ Response = ["I'm rubber you're glue. What you say bounces off me and sticks to y
             "That uni-brow keeps the Sun out of your eyes, doesn't it?", "Your so ugly, your parents got divorced.", "I hear your Momma calling!", 
             "You're just like your father, short in height and intellect.", "Was that it? Are a vegetable?", "Wait let me dumb it down for you.....DUMB-ASS.",
             "Is that your breath or did someone fart?", "WHAT!  ARE YOU CRYING? There's crying in $h*t talking!", 
-            "Well I was down at the store yesterday, and guess what I saw...YOur sister working the corner!", "Where did you go to school? The Prestigious Academy of Dumb as a Bag of Hammers?",
+            "Well I was down at the store yesterday, and guess what I saw...Your sister working the corner!", "Where did you go to school? The Prestigious Academy of Dumb as a Bag of Hammers?",
             ]
 
 #opening header
