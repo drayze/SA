0883b57419deb8a570d23ef8d68bae0ffcbec67c
Paul Slinkard <slinkardpaul50@gmail.com>
2024-08-30 16:00:10 -0500

    created opening page

---
+++ SelectALevel.py
@@ -0,0 +1,13 @@
+#First page player sees. Prompt them to select a level
+
+#Grab the player's level
+usersLevel = input("\nWAR OF WORDS\n\nPlease choose your level: \n")
+
+#process user's level input
+if usersLevel == 1:
+    #move to file SmartAss.py
+    pass
+elif usersLevel == 2:
+    #if scoreboard > 500:
+        #move to SA_level2.py
+        pass
